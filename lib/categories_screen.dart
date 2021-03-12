45ebdd25ee8662c6687b4eca1782b96dbfab1224
LAPTOP-0261QBGC\nikun <nikunjsaraf2001@gmail.com>
2021-03-12 22:06:09 +0530

    Grid creation

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+
+import './dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
@@ -6,13 +9,18 @@ class CategoriesScreen extends StatelessWidget {
     return GridView(
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
-        childAspectRatio: 3/2,  
+        childAspectRatio: 3 / 2,
         crossAxisSpacing: 20, // Vertical space between columns
-        mainAxisSpacing: 20,  // Horizontal space between rows
+        mainAxisSpacing: 20, // Horizontal space between rows
       ),
-      children: [
-        
-      ],
+      children: DUMMY_CATEGORIES
+          .map(
+            (category) => CategoryItem(
+              category.title,
+              category.color,
+            ),
+          )
+          .toList(),
     );
   }
 }
