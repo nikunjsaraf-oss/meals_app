620def495fa93f6897309fd5fd7350c03965c39a
LAPTOP-0261QBGC\nikun <nikunjsaraf2001@gmail.com>
2021-03-14 01:54:14 +0530

    Finished detail's page

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Meals App',
       theme: ThemeData(
         primarySwatch: Colors.pink,
-        accentColor: Colors.pink,
+        accentColor: Colors.amber,
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
